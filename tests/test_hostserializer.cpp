91e2ecefe72e6ff6635d30282d31c3a7ff2db515
Arew <noobterr@yandex.ru>
2019-03-11 12:33:24 +0300

    Tests to serialize method of hostserializer.

--- tests/test_hostserializer.cpp
+++ tests/test_hostserializer.cpp
@@ -46,3 +46,25 @@ void cstests::Test_HostSerializer::deserialize()
     QFile::remove(fileName);
     QCOMPARE(expectedList, deserializedList);
 }
+
+void cstests::Test_HostSerializer::serialize()
+{
+    cs::Hosts expectedList;
+    expectedList.append(cs::Host {"192.168.0.1", 6000});
+    expectedList.append(cs::Host {"192.168.0.2", 5555});
+    expectedList.append(cs::Host {"1.7.33.222", 65555});
+    expectedList.append(cs::Host {"23.13.15.555", 8080});
+
+    const QString fileName = "test.txt";
+
+    cs::HostSerializer serializer;
+    serializer.serialize(expectedList, fileName);
+
+    auto list = serializer.deserialize(fileName);
+
+    if (QFile::exists(fileName)) {
+        QFile::remove(fileName);
+    }
+
+    QCOMPARE(list, expectedList);
+}
--- tests/test_hostserializer.hpp
+++ tests/test_hostserializer.hpp
@@ -14,6 +14,7 @@ namespace cstests
 
     private slots:
         void deserialize();
+        void serialize();
     };
 }
 
