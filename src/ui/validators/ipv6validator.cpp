49d0ff1dcb32ef8c7ce9c10e6fb7c1d0c5eef019
Arew <noobterr@yandex.ru>
2019-05-27 13:50:42 +0300

    Update windows build

--- src/ui/validators/ipv6validator.cpp
+++ src/ui/validators/ipv6validator.cpp
@@ -5,6 +5,8 @@
 cs::Ipv6Validator::Ipv6Validator(QObject* parent):
     QRegularExpressionValidator(parent)
 {
-    QRegularExpression ipv6Regexpr(R"RX(\b[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}\b)RX");
+    QRegularExpression ipv6Regexpr(
+        R"RX(\b[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}:[0-9a-fA-f]{1,4}\b)RX");
+
     setRegularExpression(ipv6Regexpr);
 }
